3b10595218aa1735b755d023c54e50dbba5af731
mrmps <41216139+mrmps@users.noreply.github.com>
2023-05-05 06:54:04 -0700

    Remove rate limit

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -13,18 +13,6 @@ if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
 
-if (!process.env.UPSTASH_URL) {
-  throw new Error("Missing env var from Upstash");
-}
-
-if (!process.env.UPSTASH_TOKEN) {
-  throw new Error("Missing env var from Upstash");
-}
-
-const redis = new Redis({
-  url: process.env.UPSTASH_URL,
-  token: process.env.UPSTASH_TOKEN,
-});
 
 
 
@@ -32,34 +20,13 @@ const redis = new Redis({
 
 
 
-// Create a new ratelimiter, that allows 20 requests per day, which has this many seconds: 86400
-const ratelimit = redis
-    ? new Ratelimit({
-        redis: redis,
-        limiter: Ratelimit.fixedWindow(17, "1 d"),
-        analytics: true,
-    })
-    : undefined;
+
 
 
 const handler = async (req: Request): Promise<Response> => {
 
-  if (ratelimit) {
-    // console.log(req);
-    // const identifier = requestIp.getClientIp(req);
-    const identifier = ipAddress(req) || 'unknown';
+  
     
-    console.log('identifier:', identifier)
-    if (identifier) {
-        const result = await ratelimit.limit(identifier!);
-        console.log('result:', result)
-
-        if (!result.success) {
-            return new Response("🙏🙏 Too many requests, you get 17 per day");
-        }
-    }
-}
-
 
 
   const { prompt } = (await req.json()) as {
